25e05cb281689ca575307c13ae67fcb48a39e4e9
Egor Panteleev <jasonsuper04@gmail.com>
2024-04-28 22:41:36 +0300

    fix: absolute path to serve static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApplicationsModule } from './applications/applications.module';
@@ -14,7 +15,7 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: 'uploads',
+      rootPath: join(__dirname, '..', '..', 'uploads'),
       serveRoot: '/uploads',
     }),
     ConfigModule.forRoot({
